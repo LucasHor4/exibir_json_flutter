19f5e2b1bcb32aae5b098f01f27838e3a130e7c6
LucasHor4 <lucash.arcanjo267@gmail.com>
2025-03-19 12:05:18 -0300

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,17 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => TelaInicial();
+}
+
+class TelaInicial extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Teste de commit'),
