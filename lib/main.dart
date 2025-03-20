07ffd886ce355c9234719899c59eeaad1cc55dab
LucasHor4 <lucash.arcanjo267@gmail.com>
2025-03-20 17:22:41 -0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:exibir_json_flutter/functions/functions.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
@@ -21,6 +23,19 @@ List<testeClass> listaTeste = [
 
 class TelaInicial extends State<MainApp> {
   int muda = 0; 
+  int total = 0; 
+  List<Documentarios> docList = List.empty();
+
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/documentarios.json');
+     Iterable data = await json.decode(response);
+    docList =  List<Documentarios>.from(data.map((model)=> Documentarios.fromJson(model)));
+    total = docList.length;
+    setState(() {
+      docList;
+      total;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +46,7 @@ class TelaInicial extends State<MainApp> {
             children: [
               Container(
                 margin: EdgeInsets.only(top: 12, bottom: 20),
-                child: Text(listaTeste[muda].nome),
+                child: Text(docList[muda].titulo),
               ),
               ElevatedButton(onPressed: (){
                 //fazer if e else para caso atingir o maximo da capacidade ele voltar para 0
